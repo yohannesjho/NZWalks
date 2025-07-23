4cbf7ef4eedd41cdaa88aca108238c88e3a4fe40
yohannesjho <jhonians@outlook.com>
2025-07-23 21:19:00 +0300

    Add project files.

---
+++ NZWalks.Api/Data/NZWalksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.Api.Models.Domains;
+
+namespace NZWalks.Api.Data
+{
+    public class NZWalksDbContext: DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walks> Walks { get; set; }
+    }
+}
---
+++ NZWalks.Api/Models/Domains/Walks.cs
@@ -0,0 +1,23 @@
+﻿namespace NZWalks.Api.Models.Domains
+{
+    public class Walks
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double LengthInKm { get; set; }
+
+        public string? WalkImageUrl { get; set; }
+
+        public Guid DifficultyId { get; set; }
+
+        public Guid RegionId { get; set; }
+
+
+        public Difficulty Difficulty { get; set; }
+        public Region Region { get; set; }
+     }
+}
